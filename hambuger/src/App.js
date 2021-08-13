1ef1b263b96a27dac9950ec33dceaa2cc638aa52
anhtoan260497 <anhtoan260497@gmail.com>
2021-08-13 19:20:34 +0700

    hamburger

---
+++ hambuger/src/App.js
@@ -0,0 +1,12 @@
+
+import HambugerOrder from './Components/HambugerOrder';
+
+function App() {
+  return (
+    <div className="App">
+      <HambugerOrder />
+    </div>
+  );
+}
+
+export default App;
---
+++ hambuger/src/Components/Meat.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Meat extends Component {
+    
+    render() {
+        return (
+            <div className="meat"/>
+        );
+    }
+}
+
+export default Meat;
